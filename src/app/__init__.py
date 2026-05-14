ae2e62546ba27d8baf8b31fa2177855730c203dd
xsismadn3ss <2477carlos.artiga@gmail.com>
2026-05-14 13:22:58 -0600

    🚀 Agregar configuracion para modo de desarrollo y produccion

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 
+from src.app.config import get_config
 from src.app.routes import health, products
 
-app = FastAPI()
+config = get_config()
+
+is_dev = config.env == "development"
+
+app = FastAPI(
+    docs_url="/docs" if is_dev else None,
+    redoc_url="/redoc" if is_dev else None,
+    openapi_url="/openapi.json" if is_dev else None,
+)
 
 app.include_router(products.router)
 app.include_router(health.router)
