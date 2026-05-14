f9dbb896176c4aee0c8049a94849f757e8c405bb
xsismadn3ss <2477carlos.artiga@gmail.com>
2026-05-13 19:21:54 -0600

    ♻️ Agregar campo de contraseña
    Agregar campo de contraseña para el modelos y al momento de servir el modelo, eliminar la clave password para no exponerlo en la API

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,4 +1,7 @@
+from typing import Any, Callable, Literal
+
 from pydantic import BaseModel, Field
+from pydantic.main import IncEx
 
 
 class ProductProperty(BaseModel):
@@ -44,4 +47,43 @@ class User(BaseModel):
     id: str = Field()
     name: str = Field()
     email: str = Field()
+    password: str = Field()
     cart: Cart = Field()
+
+    def model_dump(
+        self,
+        *,
+        mode: Literal["json", "python"] | str = "python",
+        include: IncEx | None = None,
+        exclude: IncEx | None = None,
+        context: Any | None = None,
+        by_alias: bool | None = None,
+        exclude_unset: bool = False,
+        exclude_defaults: bool = False,
+        exclude_none: bool = False,
+        exclude_computed_fields: bool = False,
+        round_trip: bool = False,
+        warnings: bool | Literal["none", "warn", "error"] = True,
+        fallback: Callable[[Any], Any] | None = None,
+        serialize_as_any: bool = False,
+        polymorphic_serialization: bool | None = None,
+    ) -> dict[str, Any]:
+        data = super().model_dump(
+            mode=mode,
+            include=include,
+            exclude=exclude,
+            context=context,
+            by_alias=by_alias,
+            exclude_unset=exclude_unset,
+            exclude_defaults=exclude_defaults,
+            exclude_none=exclude_none,
+            exclude_computed_fields=exclude_computed_fields,
+            round_trip=round_trip,
+            warnings=warnings,
+            fallback=fallback,
+            serialize_as_any=serialize_as_any,
+            polymorphic_serialization=polymorphic_serialization,
+        )
+        # eliminar clave password
+        data.pop("password", None)
+        return data
