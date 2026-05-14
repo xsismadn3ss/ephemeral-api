5c0532e41f1a5a7581b0c04bd576a5d00efa3992
xsismadn3ss <2477carlos.artiga@gmail.com>
2026-05-14 15:24:08 -0600

    🐛 Agregar campo hash al registrar una comprea
    No se agregaba ese campo al diccionario, eso generaba un error Internal Server Error en produccion

--- src/app/services/receipt.py
+++ src/app/services/receipt.py
@@ -51,6 +51,7 @@ def create(
     data_dict["products_data"] = encrypted
     data_dict["total"] = total
     data_dict["data"] = datetime.now()
+    data_dict["hash"] = None
 
     result = db.receipts.insert_one(data_dict)
 
