ac469822bd3ac49f5d4faaace7e6f81fd22ab8a0
xsismadn3ss <2477carlos.artiga@gmail.com>
2026-05-14 18:11:28 -0600

    ⚡️ Agregar Depends para resolver dependencias con fastapi

--- src/app/routes/products.py
+++ src/app/routes/products.py
@@ -1,28 +1,23 @@
-from typing import List
+from typing import Annotated, List
 
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status
 
 from src.app.config import APP_Config
 from src.app.models import Product, ProductInput
 from src.app.services import products as products_service
-from src.app.utils.mongo import get_db
+from src.app.utils.mongo import Database, get_db
 
 router = APIRouter(prefix="/products", tags=["products"])
 
 
 @router.get("/", response_model=List[Product])
-async def list_products():
-    config = APP_Config()
-    db = get_db(config)
+async def list_products(db: Annotated[Database, Depends(get_db)]):
     return products_service.list_products(db)
 
 
 # obtener producto por id
 @router.get("/{product_id}", response_model=Product)
-async def get_product(product_id: str):
-    config = APP_Config()
-    db = get_db(config)
-
+async def get_product(product_id: str, db: Annotated[Database, Depends(get_db)]):
     product = products_service.get_product(db, product_id)
     if product is None:
         raise HTTPException(
@@ -32,10 +27,7 @@ async def get_product(product_id: str):
 
 
 @router.post("/")
-async def add_product(product: ProductInput):
-    config = APP_Config()
-    db = get_db(config)
-
+async def add_product(product: ProductInput, db: Annotated[Database, Depends(get_db)]):
     # Agregar producto a la base de datos
     reference = products_service.create_product(db, product.model_dump())
     # Enviar producto agregado
@@ -44,10 +36,9 @@ async def add_product(product: ProductInput):
 
 # Editar producto por id
 @router.put("/{product_id}")
-async def update_product(product_id: str, product: ProductInput):
-    config = APP_Config()
-    db = get_db(config)
-
+async def update_product(
+    product_id: str, product: ProductInput, db: Annotated[Database, Depends(get_db)]
+):
     result = products_service.update_product(db, product_id, product.model_dump())
     if result == 0:
         raise HTTPException(
@@ -60,10 +51,7 @@ async def update_product(product_id: str, product: ProductInput):
 
 # eliminar producto por id
 @router.delete("/{product_id}")
-async def delete_product(product_id: str):
-    config = APP_Config()
-    db = get_db(config)
-
+async def delete_product(product_id: str, db: Annotated[Database, Depends(get_db)]):
     result = products_service.delete_product(db, product_id)
     if result == 0:
         raise HTTPException(
